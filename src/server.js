75326869dae1e983d83bcd79624adec7a2aa97f0
avecenob <aryabasc@gmail.com>
2023-09-25 17:37:16 +0700

    add routes and handler modules for CRUD

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 const Hapi = require("@hapi/hapi");
+const routes = require("./routes");
 
 const init = async () => {
 
     const server = Hapi.server({
         port: 5000,
-        host: "localhost"
+        host: "localhost",
+        routes: {
+            cors: {
+                origin: ["*"]
+            }
+        }
     });
+
+    server.route(routes);
 
     await server.start();
     console.log(`Server is running on ${server.info.uri}`);
